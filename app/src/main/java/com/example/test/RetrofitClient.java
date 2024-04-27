d460d7067baa3c34054c61968748dfc74ec1d7d5
Bnchi <bnchifahmi@gmail.com>
2024-04-28 00:12:43 +0300

    Change url again

--- app/src/main/java/com/example/test/RetrofitClient.java
+++ app/src/main/java/com/example/test/RetrofitClient.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    //private static final String BASE_URL = "https://android-project-umber.vercel.app/";
-    private static final String BASE_URL = "http://192.168.1.20:3000/";
+    private static final String BASE_URL = "https://android-project-umber.vercel.app/";
+    //private static final String BASE_URL = "http://192.168.1.20:3000/";
 
     public static Retrofit getClient() {
         if (retrofit == null) {
